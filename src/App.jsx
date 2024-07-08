8b069fe3c368a968c5243b2b1c2e25b88c6cc394
Sine Jespersen <sinejespersen@gmail.com>
2024-07-08 20:46:06 +0200

    lint + json5 + simple app added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import './App.css'
+import { useState } from "react";
+import config from "../public/config.json5";
+import "./App.css";
 
 function App() {
+  const { pages } = config;
+  const [selectedFrame, setSelectedFrame] = useState(null);
+
+  function changeFrame(page) {
+    setSelectedFrame(page);
+  }
 
   return (
     <>
-   Hello world
+      {selectedFrame && selectedFrame.type === "audiobook" && (
+        <img src={selectedFrame.url}></img>
+      )}
+      {pages && (
+        <div>
+          {pages.map((page) => {
+            const { id, buttonText } = page;
+            return (
+              <button type="button" onClick={() => changeFrame(page)} key={id}>
+                {buttonText}
+              </button>
+            );
+          })}
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslintPlugin from "vite-plugin-eslint";
+import json5Plugin from "vite-plugin-json5";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    json5Plugin(),
+    eslintPlugin({
+      cache: false,
+      include: ["./src/**/*.js", "./src/**/*.jsx"],
+      exclude: [],
+    }),
+  ],
+});
